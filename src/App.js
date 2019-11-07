ba045db1b18c2a64085e7561da4c4c5f1462200e
close852 <close852@naver.com>
2019-11-07 17:56:59 +0900

    webpack 설정 샘플 프로젝트

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import MyUploadAdapter from './MyUploadAdapter'
 
 function App() {
+  const editConfig = {
+    // plugins: [Heading, Bold, Italic, Essentials, Paragraph, Alignment, Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, ImageResize],
+    // toolbar: [ 'heading', '|', 'bold', 'italic', '|', 'undo', 'redo', ]
+    // toolbar: ['heading', '|', 'bold', 'italic', '|', 'undo', 'redo', '|', 'alignment:left', 'alignment:center', 'alignment:right', 'alignment:justify']
+    toolbar: ['heading', '|', 'bold', 'italic', '|', 'link', 'bulletedList', 'numberedList', 'blockQuote', '|', 'imageUpload', 'insertTable', '|', 'undo', 'redo', '|', 'alignment:left', 'alignment:right', 'alignment:center', 'alignment:justify'],
+    image: {
+      toolbar: ['imageTextAlternative', '|', 'imageStyle:alignLeft', 'imageStyle:full', 'imageStyle:alignRight'],
+      resizeUnit: 'px',
+      styles: [
+        // This option is equal to a situation where no style is applied.
+        'full',
+        // This represents an image aligned to the left.
+        'alignLeft',
+        // This represents an image aligned to the right.
+        'alignRight'
+      ]
+
+    },
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CKEditor
+        onInit={(editor) => {
+          editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
+            return new MyUploadAdapter(loader);
+          };
+
+        }
+
+        }
+        editor={ClassicEditor}
+        config={editConfig}
+        data="<p>Hello form CKEditor 5!</p>"
+      />
     </div>
   );
 }
